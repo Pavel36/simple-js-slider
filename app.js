7c01c315e0f5702da2f8b062257ba6dacec5065a
Pablo <pgocev@mail.ru>
2021-09-21 16:33:49 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+function slidesPlugin(activeSlideIndex=1) {
+    const slides = document.querySelectorAll('.slide')
+    slides[activeSlideIndex].classList.add('active')
+
+    for (const slide of slides) {
+        slide.addEventListener('click',()=>{
+            clearActiveClasses()
+            slide.classList.add('active')
+        })
+    }
+
+    function clearActiveClasses() {
+        slides.forEach(slide=>slide.classList.remove('active'))
+    }
+}
+
+slidesPlugin(2)
